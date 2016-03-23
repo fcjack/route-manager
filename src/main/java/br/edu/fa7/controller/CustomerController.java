2789b40407f32ad16319730fae75f31cad1f3b55
Jackson Coelho <fcjack@gmail.com>
2016-03-23 20:40:34 -0300

    Crud de customer

--- src/main/java/br/edu/fa7/controller/CustomerController.java
+++ src/main/java/br/edu/fa7/controller/CustomerController.java
@@ -1,36 +1,60 @@
 package br.edu.fa7.controller;
 
+import br.edu.fa7.dao.CustomerDao;
+import br.edu.fa7.dao.EnterpriseCustomerDao;
+import br.edu.fa7.dao.NormalCustomerDao;
 import br.edu.fa7.domain.Customer;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import br.edu.fa7.domain.EnterpriseCustomer;
+import br.edu.fa7.domain.NormalCustomer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class CustomerController {
+
+    @Autowired
+    private NormalCustomerDao normalCustomerDao;
+
+    @Autowired
+    private EnterpriseCustomerDao enterpriseCustomerDao;
+
+    @Autowired
+    private CustomerDao customerDao;
 
     @RequestMapping("/customer")
     public List<Customer> list() {
-        return null;
+        return customerDao.list();
     }
 
     @RequestMapping("/customer/{id}")
     public Customer get(@PathVariable("id") Integer id) {
-        return null;
+        return customerDao.findById(id);
     }
 
-    @RequestMapping(value = "/customer", method = RequestMethod.POST)
-    public Customer create(Customer customer) {
-        return null;
+    @RequestMapping(value = "/customer/saveNormalCustomer", method = RequestMethod.POST)
+    public void create(@RequestBody NormalCustomer customer) {
+        normalCustomerDao.save(customer);
     }
 
-    @RequestMapping(value = "/customer", method = RequestMethod.PUT)
-    public Customer update(Customer customer) {
-        return null;
+    @RequestMapping(value = "/customer/saveEnterpriseCustomer", method = RequestMethod.POST)
+    public void create(@RequestBody EnterpriseCustomer customer) {
+        enterpriseCustomerDao.save(customer);
     }
 
-    @RequestMapping(value = "/customer/{id}", method = RequestMethod.DELETE)
-    public Boolean delete(@PathVariable("id") Integer id) {
-        return null;
+    @RequestMapping(value = "/customer/saveNormalCustomer", method = RequestMethod.PUT)
+    public void update(@RequestBody NormalCustomer customer) {
+        normalCustomerDao.save(customer);
+    }
+
+    @RequestMapping(value = "/customer/saveEnterpriseCustomer", method = RequestMethod.PUT)
+    public void update(@RequestBody EnterpriseCustomer customer) {
+        enterpriseCustomerDao.save(customer);
+    }
+
+    @RequestMapping(value = "/customer/{customerId}", method = RequestMethod.DELETE)
+    public void create(@PathVariable Integer customerId) {
+        enterpriseCustomerDao.remove(customerId);
     }
 }
